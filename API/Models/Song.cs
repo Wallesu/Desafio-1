89028597b9dad4dc3e5e2f37d1a0b7c03f7281f4
wallyson.souza <wallyson.souza@FULL-101>
2024-01-16 14:45:16 -0300

    - Propriedades desnecessárias removidas do Model "Song".

--- API/Models/Song.cs
+++ API/Models/Song.cs
@@ -8,13 +8,6 @@ namespace API.Models
         public List<Genre> Genres { get; set; }
         public string Name { get; set; }
         public string Year { get; set; }
-        public int DurationMs { get; set; }
-        public string Popularity { get; set; }
-        public string Danceability { get; set; }
-        public string Energy { get; set; }
-        public int Key { get; set; }
-        public string Loudness { get; set; }
-        public string Mode { get; set; }
 
     }
 }
