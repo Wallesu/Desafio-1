a32bf4c330436ab59067687f23898d3ed71dc4a5
wallyson.souza <wallyson.souza@FULL-101>
2024-01-17 08:31:42 -0300

    - Adicionado configurações para o CORS.

--- API/Program.cs
+++ API/Program.cs
@@ -1,7 +1,19 @@
 using API.Data;
 using API.Services;
 
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("http://localhost:8080",
+                                              "http://192.168.40.4:8080");
+                      });
+});
 
 // Add services to the container.
 
@@ -23,6 +35,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseAuthorization();
 
